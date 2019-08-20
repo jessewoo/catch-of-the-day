ad8f51d12f65e487adbafcbe46fe13ab63808fb0
Jesse Woo <woojesse@gmail.com>
2019-08-20 15:17:41 -0700

    Remove API

--- src/base.js
+++ src/base.js
@@ -2,9 +2,6 @@ import Rebase from 're-base';
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC4GD3tQAjQ4lI8aKwrdse74vSrvD5SD04",
-    authDomain: "catch-of-the-day-jesse-woo.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-jesse-woo.firebaseio.com",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
